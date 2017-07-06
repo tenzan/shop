a63444e7ed5d38f04a640194aa037dd440a5699e
Askar Karasaev <askar.karasaev@gmail.com>
2017-07-06 09:35:51 +0900

    Viewing a product for a specific category

---
+++ spec/features/viewing_products_spec.rb
@@ -0,0 +1,30 @@
+feature 'Users can view products' do
+  before do
+    path_01 =  File.join(Rails.root, "/spec/fixtures/images/sample_image_01.png")
+    path_02 =  File.join(Rails.root, "/spec/fixtures/images/sample_image_02.png")
+
+    books = FactoryGirl.create(:category, name: 'Books')
+    FactoryGirl.create(:product, category: books, name: 'Sample book title',
+                       description: 'Some description for sample book',
+                       price: 23.00, image: Rack::Test::UploadedFile.new(path_01))
+
+     magazines = FactoryGirl.create(:category, name: 'Magazines')
+     FactoryGirl.create(:product, category: magazines, name: 'Sample magazine title',
+                        description: 'Some description for sample magazine',
+                        price: 43.00, image: Rack::Test::UploadedFile.new(path_02))
+
+     visit '/'
+  end
+  scenario 'for a given category' do
+    click_link 'Books'
+
+    expect(page).to have_content 'Sample book title'
+    expect(page).to_not have_content 'Sample magazine title'
+
+    click_link 'Sample book title'
+    within("#product h2") do
+      expect(page).to have_content 'Sample book title'
+    end
+    expect(page).to have_content "Sample book title"
+  end
+end
