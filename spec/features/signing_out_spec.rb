5958662adbad4578ecee2aeabce6d2a6a556a168
Askar Karasaev <askar.karasaev@gmail.com>
2017-07-06 13:42:01 +0900

    Add sign in and sign out

---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,11 @@
+feature 'Sined-in users can sign out' do
+  let!(:user) { FactoryGirl.create(:user) }
+  before do
+    login_as(user)
+  end
+  scenario do
+    visit "/"
+    click_link "Sign out"
+    expect(page).to have_content "Signed out successfully."
+  end
+end
